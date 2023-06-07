2c02cecfde334f0c818dd1c2abbda69fc4485b33
Rex Meeks <rex@repowr.com>
2023-06-07 16:28:48 -0500

    initial files

---
+++ src/main/java/com/weinfuse/beassessment/WeInfuseBeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.weinfuse.beassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeInfuseBeAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeInfuseBeAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weinfuse/beassessment/WeInfuseBeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weinfuse.beassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeInfuseBeAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
